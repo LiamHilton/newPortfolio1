c7cb588e93cc9ffa26f9f1363b15fbf8bb2aedc7
Liam Hilton <liamhiltoncode@gmail.com>
2025-02-15 10:49:12 +1100

    config deploy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,6 +22,7 @@ const Navbar = () => {
     <div className="navbar">
         <div className="navbar-links">
             <div className="navbar-links-logo">
+              {/* i could add liam hilton to a link to navigate back to home page */}
                 <h1>Liam Hilton <br /> <span>Web developer & UX/UI</span></h1>
             </div>
             <div className="navbar-links-container">
